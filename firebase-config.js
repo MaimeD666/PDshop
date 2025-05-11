6863809b87caa4e4546f30894e1602c0529b75b7
MaimeD666 <banovichban0@gmail.com>
2025-05-11 11:28:53 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Конфигурация Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCDjXMxt7ppjd4rgEdQvdkXMYqz2wOLz3Y",
+  authDomain: "pdshop-8f846.firebaseapp.com",
+  projectId: "pdshop-8f846",
+  storageBucket: "pdshop-8f846.firebasestorage.app",
+  messagingSenderId: "673934870613",
+  appId: "1:673934870613:web:bfa384823780a5e8a1acdc"
 };
 
 // Инициализация Firebase
@@ -13,4 +13,3 @@ firebase.initializeApp(firebaseConfig);
 
 // Создание ссылок на сервисы
 const db = firebase.firestore();
-const storage = firebase.storage();
